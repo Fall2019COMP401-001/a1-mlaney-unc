2ed00aea01961e2a8b3bc36f1423692275c2cbd6
mason <mason@MasonLaney-Thinkpad.wireless-1x.unc.edu>
2019-08-30 15:59:06 -0400

    test

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,14 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		int numCustomers = scan.nextInt();
+		
+		for (int i = 0; i < numCustomers; ++i) {
+			
+			String name = scan.next().charAt(0) + ". " + scan.next();
+			
+			
+		}
 		
 	}
 }
