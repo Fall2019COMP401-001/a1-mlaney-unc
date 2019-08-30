f714735e73c773d5d56b5b81968819e70c973908
mason <mason@MasonLaney-Thinkpad.wireless-1x.unc.edu>
2019-08-30 16:24:21 -0400

    final version for A1 novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,8 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		int numCustomers = scan.nextInt();
 		
+		int numCustomers = scan.nextInt();
 		for (int i = 0; i < numCustomers; ++i) {
 			
 			String name = scan.next().charAt(0) + ". " + scan.next();
